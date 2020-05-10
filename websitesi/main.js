628e1637b01c60fa1a3098b395bb701084a233bd
Szg7946 <62610069+Szg7946@users.noreply.github.com>
2020-05-10 17:46:00 +0300

    Add files via upload
    Son düzenlemeler(Temalar eklendi.)

---
+++ websitesi/main.js
@@ -0,0 +1,53 @@
+function ana()
+{
+    document.documentElement.setAttribute('data-theme','ana');
+}
+function kirmizi()
+{
+    document.documentElement.setAttribute('data-theme','kirmizi');
+}
+function mavi()
+{
+    document.documentElement.setAttribute('data-theme','mavi');
+}
+function koyu()
+{
+    document.documentElement.setAttribute('data-theme','koyu');
+}
+function acik()
+{
+    document.documentElement.setAttribute('data-theme','acik');
+}
+function sari()
+{
+    document.documentElement.setAttribute('data-theme','sari');
+}
+function mor()
+{
+    document.documentElement.setAttribute('data-theme','mor');
+}
+
+function hakkimda()
+{
+    document.getElementById("header").innerHTML="HAKKIMDA";
+}
+function ozgecmis()
+{
+    document.getElementById("header").innerHTML="ÖZGEÇMİŞ";
+}
+function sehrim()
+{
+    document.getElementById("header").innerHTML="ŞEHRİM";
+}
+function mirasimiz()
+{
+    document.getElementById("header").innerHTML="MİRASIMIZ";
+}
+function iletisim()
+{
+    document.getElementById("header").innerHTML="İLETİŞİM";
+}
+function giris()
+{
+    document.getElementById("header").innerHTML="GİRİŞ";
+}
